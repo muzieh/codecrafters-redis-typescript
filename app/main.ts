20730742e41d1321e4b937d8f057d98f859c1108
Marcin Owerczuk <muzieh@gmail.com>
2025-02-11 04:51:55 +0100

    multiple pings

--- app/main.ts
+++ app/main.ts
@@ -6,8 +6,10 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
   console.log('somebody connected ');
-  connection.write("+PONG\r\n");
-  // Handle connection
+  connection.on("data", (data: string) => {
+    console.log(`received: ${data}`);
+    connection.write("+PONG\r\n");
+  })
 });
 
 server.listen(6379, "127.0.0.1");
