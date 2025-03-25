bbb9183d6ea91a194dd13d8888d28115535da557
Marcin Owerczuk <marcin.owerczuk@checkatrade.com>
2025-03-25 22:04:39 +0100

    log expiration

--- app/commands/get.ts
+++ app/commands/get.ts
@@ -4,8 +4,10 @@ import { createBulkString } from "../input-parser";
 export function getCommand(input: string[], store: Store<string>): string {
   const value = store[input[1]];
   if(!value.expires || (value.expires > new Date())) {
+    console.log(`Key ${value} didn't expire`)
     return createBulkString(value.data);
   } else {
+    console.log(`Key ${value} has expired`)
     return createBulkString(undefined);
   }
 }
