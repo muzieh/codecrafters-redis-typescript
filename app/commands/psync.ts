9eacf95f1ce19dfe84857f19bbf7f3e5e5908603
Marcin Owerczuk <muzieh@gmail.com>
2025-03-27 11:49:49 +0100

    codecrafters submit [skip ci]

--- app/commands/psync.ts
+++ app/commands/psync.ts
@@ -1,5 +1,6 @@
 export function psyncCommand(input: string[]): string {
   console.log(`psync ${input}`);
-  return "+FULLRESYNC <REPL_ID> 0\r\n";
+  const replid = "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb";
+  return `+FULLRESYNC ${replid} 0\r\n`;
 }
 
